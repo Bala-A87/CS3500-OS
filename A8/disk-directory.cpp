437a52dae16a83ed2163633c5cb62b882ce924c6
Bala-A87 <bala8702@gmail.com>
2022-11-02 12:55:22 +0530

    Completed A8

--- A8/disk-directory.cpp
+++ A8/disk-directory.cpp
@@ -12,7 +12,6 @@ using namespace std;
 
 const int BLOCK_SIZE = 128;
 const int KILO = 1024;
-const int INODE_INFO_SIZE = 93;
 const string LOAD = "load";
 const string DELETE = "delete";
 const string PRINT = "print";
@@ -49,8 +48,38 @@ void print_no_space_error() {
     cout<<"ERROR: Disk has insufficient space for this operation"<<endl;
 }
 
-void file_dne_error(string filename) {
+void print_file_dne_error(string filename) {
     cout<<"ERROR: File "<<filename<<" does not exist"<<endl;
+}
+
+bool check_filename_validity(string filename) {
+    if(filename[0] != '/') return false;
+    if(filename.size() > 16) return false;
+    int slash_count = 0, dot_count = 0;
+    for(char curr_char : filename) {
+        if(curr_char == '/') slash_count++;
+        if(curr_char == '.') dot_count++;
+    }
+    if(slash_count > 2 || dot_count > 1) return false;
+    int directory_name_size = 0;
+    int file_name_size = 0;
+    int extension_size = 0;
+    int curr_index = 0;
+    if(slash_count == 2) {
+        for(curr_index = 1; filename[curr_index] != '/'; curr_index++) {
+            directory_name_size++;
+            if(filename[curr_index] == '.') return false;
+        }
+    }
+    for(curr_index = curr_index+1; curr_index < filename.size(); curr_index++) {
+        if(filename[curr_index] == '.') break;
+        file_name_size++;
+    }
+    if(curr_index < filename.size()) {
+        for(curr_index = curr_index+1; curr_index < filename.size(); curr_index++) extension_size++;
+    }
+    if(directory_name_size > 4 || extension_size > 3) return false;
+    return true;
 }
 
 class inode {
@@ -69,7 +98,7 @@ public:
     inode(string file_name) {
         this->file_name = file_name;
         type = false;
-        file_size = INODE_INFO_SIZE;
+        file_size = BLOCK_SIZE;
         date_created = get_time();
         date_last_modified = date_created;
         direct_block_addresses[0] = get_free_block();
@@ -110,9 +139,11 @@ public:
         for(int used_block : index_block) free_blocks.push(used_block);
     }
 
-    void blocks_reqd_for_append(string append_data) {
+    int blocks_reqd_for_append(string append_data) {
         int curr_no_blocks = no_blocks_used;
-        // Check!! Need to check whether index block is allocated too 
+        int new_no_blocks = 1 + ((file_data+append_data).size() + BLOCK_SIZE-1)/BLOCK_SIZE;
+        if(new_no_blocks > 4) new_no_blocks++;
+        return new_no_blocks-curr_no_blocks;
     }
 
     void allocate_new_block() {
@@ -130,15 +161,15 @@ public:
             no_blocks_used++;
         }
         next_block = get_free_block();
-        // Check if file size exceeds limit
         index_block.push_back(next_block);
     }
 
     void append_contents(string append_data) {
         file_data += append_data;
         file_size += append_data.size();
-        int req_no_blocks = append_data.size()/BLOCK_SIZE;
-        for(int i = 0; i < req_no_blocks; i++) allocate_new_block();
+        int final_no_blocks = no_blocks_used + blocks_reqd_for_append(append_data);
+        while(no_blocks_used < final_no_blocks) allocate_new_block();
+        date_last_modified = get_time();
     }
 
     void display() {
@@ -150,56 +181,98 @@ public:
 int main(int argc, char *argv[]) {
     int D;
     int no_blocks;
-    // Handle cmd line args
     if(argc != 2) {
         cout<<"Please stick to specifications"<<endl;
+        cout<<"Usage: ./disk-directory <D>"<<endl;
+        cout<<"Where D is the disk size in KB"<<endl;
         exit(1);
     }
     else {
         D = atoi(argv[1]);
         no_blocks = (D * KILO)/BLOCK_SIZE;
     }
-    for(int i = D; i > 0; i--) free_blocks.push(i);
+    for(int i = no_blocks; i > 0; i--) free_blocks.push(i);
     map<string, inode*> directory;
     directory.clear();
+    cout<<"List of available commands:"<<endl;
+    cout<<"\tload <filename>"<<endl;
+    cout<<"\t\tCreates file with name <filename> and loads it onto disk"<<endl;
+    cout<<"\tdelete <filename>"<<endl;
+    cout<<"\t\tDeletes file with name <filename> from disk"<<endl;
+    cout<<"\tprint <filename>"<<endl;
+    cout<<"\t\tPrints contents of file <filename>"<<endl;
+    cout<<"\tappend <filename>"<<endl;
+    cout<<"\t\tGets data to append from user, and appends it to <filename>"<<endl;
+    cout<<"\tdisplay"<<endl;
+    cout<<"\t\tPrints name and size of files in the disk"<<endl;
+    cout<<"\tfreespace"<<endl;
+    cout<<"\t\tPrints available space in the disk"<<endl;
+    cout<<"\tprintinode <filename>"<<endl;
+    cout<<"\t\tPrints inode contents of file <filename>"<<endl;
+    cout<<"\texit"<<endl;
+    cout<<"\t\tExit the system"<<endl;
     while(true) {
         string command, filename;
+        cout<<endl<<"Enter a command to execute"<<endl;
         cin>>command;
         if(LOAD == command) {
             cin>>filename;
-            // What if filename already exists?
-            if(has_free_blocks())
-                directory[filename] = new inode(filename);
-            else 
-                print_no_space_error();
+            if(check_filename_validity(filename)) {
+                if(directory.find(filename) != directory.end()) {
+                    inode *del_file = directory[filename];
+                    directory.erase(filename);
+                    del_file->delete_file();
+                    free(del_file);
+                }
+                if(has_free_blocks()) 
+                    directory[filename] = new inode(filename);
+                else 
+                    print_no_space_error();
+            }
+            else
+                cout<<"ERROR: File name "<<filename<<" is invalid"<<endl;
         }
         else if(DELETE == command) {
             cin>>filename;
-            // Check if file exists
-            inode *del_file = directory[filename];
-            directory.erase(filename);
-            del_file->delete_file();
-            free(del_file);
+            if(directory.find(filename) != directory.end()) {
+                inode *del_file = directory[filename];
+                directory.erase(filename);
+                del_file->delete_file();
+                free(del_file);
+            }
+            else
+                print_file_dne_error(filename);
         }
         else if(PRINT == command) {
             cin>>filename;
-            // Check if file exists
-            directory[filename]->print_contents();
+            if(directory.find(filename) != directory.end())
+                directory[filename]->print_contents();
+            else
+                print_file_dne_error(filename);
         }
         else if(APPEND == command) {
             cin>>filename;
-            string append_data;
-            cout<<"Enter data to append to "<<filename<<endl;
-            cin.ignore();
-            getline(cin, append_data);
-            // Check if file exists
-            // Is there enough space to append?
-            directory[filename]->append_contents(append_data);
+            if(directory.find(filename) != directory.end()) {
+                string append_data;
+                cout<<"Enter data to append to "<<filename<<endl;
+                cin.ignore();
+                getline(cin, append_data);
+                if(has_free_blocks(directory[filename]->blocks_reqd_for_append(append_data)))
+                    directory[filename]->append_contents(append_data);
+                else
+                    print_no_space_error();
+            }
+            else
+                print_file_dne_error(filename);
         }
         else if(DISPLAY == command) {
-            // Check if empty and print special message?
-            for(auto file : directory) {
-                file.second->display();
+            if(!directory.empty()) {
+                for(auto file : directory) {
+                    file.second->display();
+                }
+            }
+            else {
+                cout<<"The directory is empty"<<endl;
             }
         }
         else if(FREESPACE == command) {
@@ -209,8 +282,10 @@ int main(int argc, char *argv[]) {
         }
         else if(PRINTINODE == command) {
             cin>>filename;
-            // Check if file exists
-            directory[filename]->print_inode();
+            if(directory.find(filename) != directory.end())
+                directory[filename]->print_inode();
+            else
+                print_file_dne_error(filename);
         }
         else if(EXIT == command)
             break;
