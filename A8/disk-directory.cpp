26f38915046b8168ca52c351cc93ada5fddffa5f
Bala-A87 <bala8702@gmail.com>
2022-11-02 08:59:29 +0530

    Minor changes

--- A8/disk-directory.cpp
+++ A8/disk-directory.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
-#include<fstream>
-#include<time.h>
 #include<vector>
 #include<string>
 #include<map>
+#include<stdlib.h>
+#include<string.h>
+#include<ctime>
+#include<chrono>
+#include<queue>
 
 using namespace std;
 
 const int BLOCK_SIZE = 128;
+const int KILO = 1024;
+const int INODE_INFO_SIZE = 93;
 const string LOAD = "load";
 const string DELETE = "delete";
 const string PRINT = "print";
@@ -17,15 +22,48 @@ const string FREESPACE = "freespace";
 const string PRINTINODE = "printinode";
 const string EXIT = "exit";
 
-typedef struct inode {
-    char file_name[16];
+queue<int> free_blocks;
+
+string get_time() {
+    auto now = chrono::system_clock::now();
+    time_t curr_time = chrono::system_clock::to_time_t(now);
+    string result = ctime(&curr_time);
+    return result.substr(0, result.size()-1);
+}
+
+bool has_free_blocks() {
+    return !free_blocks.empty();
+}
+
+int get_free_block() {
+    int free_block = free_blocks.front();
+    free_blocks.pop();
+    return free_block;
+}
+
+class inode {
+    string file_name;
     bool type;
     int file_size;
-    char date_created[28];
-    char date_last_modified[28];
+    string date_created;
+    string date_last_modified;
     int direct_block_addresses[3];
     int index_block_address;
     vector<int> index_block;
+    string file_data;
+
+public:
+    inode(string file_name) {
+        this->file_name = file_name;
+        type = false;
+        file_size = INODE_INFO_SIZE;
+        date_created = get_time();
+        date_last_modified = date_created;
+        direct_block_addresses[0] = get_free_block();
+        direct_block_addresses[1] = direct_block_addresses[2] = index_block_address = -1;
+        index_block.clear();
+        file_data = "";
+    }
 
     void print_inode() {
         cout<<"Filename: "<<file_name<<endl;
@@ -33,27 +71,47 @@ typedef struct inode {
         cout<<"Date Created: "<<date_created<<endl;
         cout<<"Date Last Modified: "<<date_last_modified<<endl;
         cout<<"Direct block values: ";
-        for(int i = 0; i < 3; i++) cout<<direct_block_addresses[i]<<" ";
+        for(int i = 0; i < 3; i++) {
+            if(direct_block_addresses[i] == -1) break;
+            cout<<direct_block_addresses[i]<<" ";
+        }
         cout<<endl;
-        cout<<"Index block is stored in: "<<index_block_address<<endl;
-        cout<<"Index block contents: ";
-        for(int block : index_block) cout<<block<<" ";
-        cout<<endl;
+        if(index_block_address != -1) {
+            cout<<"Index block is stored in: "<<index_block_address<<endl;
+            cout<<"Index block contents: ";
+            for(int block : index_block) cout<<block<<" ";
+            cout<<endl;
+        }
     }
-} inode;
+
+    void print_contents() {
+        cout<<file_data<<endl;
+    }
+};
 
 
 int main(int argc, char *argv[]) {
+    int D;
+    int no_blocks;
     // Handle cmd line args
-
-    map<string, inode> directory;
-    // Extract from Diskfile if needed
+    if(argc != 2) { //check later
+        cout<<"Please stick to specifications"<<endl;
+        exit(1);
+    }
+    else {
+        D = atoi(argv[1]);
+        no_blocks = (D * KILO)/BLOCK_SIZE;
+    }
+    for(int i = D; i > 0; i--) free_blocks.push(i);
+    map<string, inode*> directory;
+    directory.clear();
     while(true) {
         string command, filename;
         cin>>command;
         if(LOAD == command) {
             cin>>filename;
-
+            // What if filename already exists?
+            directory[filename] = new inode(filename);
         }
         else if(DELETE == command) {
             cin>>filename;
@@ -61,7 +119,7 @@ int main(int argc, char *argv[]) {
         }
         else if(PRINT == command) {
             cin>>filename;
-
+            
         }
         else if(APPEND == command) {
             cin>>filename;
